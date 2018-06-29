180ed20369bbe9b347fb40c591912a00901eddee
imorandinwnp <imorandin@wnpower.com>
2018-06-29 10:49:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h2>Linux-Config</h2>
+<p>Conjunto de scripts de post-configuraci&oacute;n recomendadas para diferentes distribuciones de Linux.</p>
+<p>Modo de uso:&nbsp;</p>
+<ul>
+<li><span>CentOS 7: wget&nbsp;</span>https://raw.githubusercontent.com/wnpower/cPanel-Linux/master/configure_centos.sh<span>&nbsp;&amp;&amp; bash configure_centos.sh</span></li>
+<li><span>M&aacute;s por venir!</span></li>
+</ul>
---
+++ configure_centos.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f /etc/redhat-release ]; then
+	echo "CentOS no detectado, abortando."
+	exit 0
+fi
+
+echo "Actualizando SO..."
+yum update -y
+yum groupinstall "Base" -y
+yum install screen -y
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
+/usr/sbin/setenforce 0
+iptables-save > /root/firewall.rules
+
+echo "Configurando Red..."
+RED=$(route -n | awk '$1 == "0.0.0.0" {print $8}')
+ETHCFG="/etc/sysconfig/network-scripts/ifcfg-$RED"
+
+sed -i '/^PEERDNS=.*/d' $ETHCFG
+sed -i '/^DNS1=.*/d' $ETHCFG
+sed -i '/^DNS2=.*/d' $ETHCFG
+	
+echo "PEERDNS=no" >> $ETHCFG
+echo "DNS1=8.8.8.8" >> $ETHCFG
+echo "DNS2=8.8.4.4" >> $ETHCFG
+
+echo "Reescribiendo /etc/resolv.conf..."
+
+echo "nameserver 8.8.8.8" > /etc/resolv.conf # Google
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf # Google
+
+
+echo "Configurando SSH..."
+sed -i 's/^X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
+sed -i 's/#UseDNS.*/UseDNS no/' /etc/ssh/sshd_config
+
+echo "Configurando FSCK..."
+grubby --update-kernel=ALL --args=fsck.repair=yes
+grep "fsck.repair" /etc/default/grub > /dev/null || sed 's/^GRUB_CMDLINE_LINUX="/&fsck.repair=yes /' /etc/default/grub
+
+echo "Configurando Yum-Cron..."
+yum -y install yum-cron
+sed -i 's/^apply_updates.*/apply_updates = yes/' /etc/yum/yum-cron.conf
+systemctl start yum-cron.service
+
+echo "Configurando SSD (de poseer)..."
+for DEVFULL in /dev/sg? /dev/sd?; do
+	DEV=$(echo "$DEVFULL" | cut -d'/' -f3)
+        if [ -f "/sys/block/$DEV/queue/rotational" ]; then
+        	TYPE=$(grep "0" /sys/block/$DEV/queue/rotational > /dev/null && echo "SSD" || echo "HDD")
+		if [ "$TYPE" = "SSD" ]; then
+			systemctl enable fstrim.timer
+
+		fi
+        fi
+done
+
+echo "Sincronizando fecha con pool.ntp.org..."
+ntpdate 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org 0.south-america.pool.ntp.org
+if [ -f /usr/share/zoneinfo/America/Buenos_Aires ]; then
+        echo "Seteando timezone a America/Buenos_Aires..."
+        mv /etc/localtime /etc/localtime.old
+        ln -s /usr/share/zoneinfo/America/Buenos_Aires /etc/localtime
+fi
+
+echo "Seteando fecha del BIOS..."
+hwclock -r
+
+echo "Instalando GIT..."
+yum install git -y
+
+echo "Finalizado!"
